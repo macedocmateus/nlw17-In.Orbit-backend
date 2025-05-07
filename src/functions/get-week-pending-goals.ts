bb72c790aa7c24809fffd897785a50ce4e403890
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-07 20:59:21 -0300

    add create goal completion

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -1,10 +1,7 @@
 import dayjs from 'dayjs'
-import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 import { and, count, lte, gte, eq, sql } from 'drizzle-orm'
-
-dayjs.extend(weekOfYear)
 
 export async function getWeekPendingGoals() {
   const firstDayOfWeek = dayjs().startOf('week').toDate()
